b161f1b3674a74c8daed1994a8f1efdb4b390cdb
Merry Mou <moumerry@gmail.com>
2016-07-13 21:35:19 -0400

    setup

---
+++ src/client/main.h
@@ -0,0 +1,19 @@
+/* 
+ * adapted from tcpsnoop code by René Pfeiffer <lynx@luchs.at>
+ */
+
+#define DEFAULT_BUFFER      1500
+#define DEFAULT_BYTES       16384
+#define DEFAULT_DAEMON      1
+#define DEFAULT_FILENAME    "mbm.log"
+#define DEFAULT_LOOPS       100
+#define DEFAULT_PORT        42237
+#define DEFAULT_SLEEP       1
+
+#define MAX_TCPOPT          256
+#define MIN_BUFFER          576
+#define MIN_BYTES           16384
+#define FILENAME_MAXLENGTH  256
+#define REPLY_MAXLENGTH     2048
+
+#define SYSLOG_IDENTITY     "mbm"
