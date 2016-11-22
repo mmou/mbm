7607d05c1792bb820f6b3047634958460d1a1e42
Steven Bauer <bauer@mit.edu>
2016-11-22 14:37:00 -0500

    updated iptables rules and tcp_info stats printed

--- src/server/cbr.cc
+++ src/server/cbr.cc
@@ -150,6 +150,7 @@ Result RunCBR(const Socket* client_mbm_socket,
                 fprintf(stdout, "tcpi_lost: %u\n", tcp_info.tcpi_lost);
                 fprintf(stdout, "tcpi_retrans: %u\n", tcp_info.tcpi_retrans);
                 fprintf(stdout, "tcpi_retransmits: %u\n", tcp_info.tcpi_retransmits);
+                fprintf(stdout, "tcpi_total_retrans: %u\n", tcp_info.tcpi_total_retrans);
 
 
 
